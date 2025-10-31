5996d8281dbcdb7b3b55715913a858f7503bea9d
Ilya Chehov <ichehoc96@gmail.com>
2025-10-31 16:02:12 +0400

    feat: install ui library PrimeVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,10 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import PrimeVueComponents from "@/primeVueComponents.js";
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -19,6 +23,12 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
+            .use(PrimeVueComponents)
             .mount(el);
     },
     progress: {
